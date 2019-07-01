7c9ce7fe15929a808c2c5bf7a70beba8e1fae115
yang <mars.yang@wisdom56.com>
2019-07-01 22:51:29 +0800

    内置角色sql

---
+++ iwcs_dao/src/main/resources/liquibase/init_insert/system/201907012230_s_role.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO `s_role` (`id`, `rolecode`, `role_name`, `is_sys_role`, `available`, `company_id`, `created_by`, `created_time`, `last_modified_by`, `last_modified_time`, `delete_flag`)
+VALUES
+	(3, 'W001', '仓库管理角色', 1, 1, 1, 1, 1553665874440, 1, 1553665874440, 0),
+	(4, 'W002', '仓库普通角色', 0, 1, 0, 1, 1553665874440, 1, 1553665874440, 0);
+
