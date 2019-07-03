37775dc023fa2dc3712fc98d0b4a1526384a3203
ted <ted.ma@wisdom56.com>
2019-07-03 16:34:30 +0800

    子任务执行器update

--- iwcs_service_biz/src/main/java/com.wisdom.iwcs.service/task/subtask/impl/SubTaskWorker.java
+++ iwcs_service_biz/src/main/java/com.wisdom.iwcs.service/task/subtask/impl/SubTaskWorker.java
@@ -7,7 +7,6 @@ import com.wisdom.iwcs.domain.task.SubTask;
 import com.wisdom.iwcs.service.task.AbstractTaskWorker;
 import com.wisdom.iwcs.service.task.conditions.ConditionBase;
 import com.wisdom.iwcs.service.task.impl.SubTaskService;
-import com.wisdom.iwcs.service.task.intf.IMainTaskService;
 import com.wisdom.iwcs.service.task.maintask.MainTaskWorker;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -30,14 +29,11 @@ public class SubTaskWorker extends AbstractTaskWorker {
 
     public boolean isRunnable(){
         // 1. 判断内存中记录的N个条件是否都已经满足，如果满足，直接返回OK
-        IMainTaskService mainTaskService = (IMainTaskService) AppContext.getBean("mainTaskService");
-        SubTaskService subTaskService = (SubTaskService) AppContext.getBean("subTaskService");
-        subTaskService.preConditionsCheckAndExec(subTask);
-
-        // 2. 判断是否有资源能满足条件，如果满足则直接锁定。
+        // 判断是否有资源能满足条件，如果满足则直接锁定。
         // 满足的条件该锁定资源需要锁定，并记录锁定状态及谁锁定了该资源, in_lock and lock_source
+        SubTaskService subTaskService = (SubTaskService) AppContext.getBean("subTaskService");
+        return subTaskService.preConditionsCheckAndExec(subTask);
 
-        return true;
     }
 
 
@@ -46,7 +42,7 @@ public class SubTaskWorker extends AbstractTaskWorker {
         while (true){
             try {
                 synchronized (waitLock){
-                    System.out.println("Main task is going to wait " + waitLock);
+                    System.out.println("sub task is going to wait " + waitLock);
                     if (! isRunnable()) {
                         waitLock.wait(10 * 1000);
                     } else{
@@ -74,33 +70,13 @@ public class SubTaskWorker extends AbstractTaskWorker {
 
     @Override
     public void process() {
-        logger.info("子任务{}执行器线程被启动", subTask.getSubTaskNum());
+
+        logger.info("子任务{}开始下发process ", subTask.getSubTaskNum());
 
         // 1. 从数据库获取子任务单
 
         // 2. 根据subtask的值，完善下发的信息，并下发命令
 
-
-
-//        List<SubTaskCondition> preTaskRelConditionsList = ;
-//        List<SubTaskCondition> afterTaskRelConditionsList = subtask.getAfterTaskRelConditionsList();
-//        while (true) {
-//            logger.info("开始检查主任务{}开始执行下一步子任务{}，检查条件", subtask.getMainTaskNum(), subtask.getSubTaskNum());
-//            //已结束的任务自动结束该任务并上报主任务线程
-//            if (SubTaskStatusEnum.Finished.getStatusCode().equals(subtask.getTaskStatus())) {
-//                //TODO 上报主线程
-//                break;
-//            } else {
-//                // 检查并执行未完成的前置条件
-//                preTaskRelConditionsList.stream().forEach(c -> c.getConditonTriger());
-//
-//
-//            }
-//
-//
-//            subtask.getConditionHandler();
-//            logger.info("开始执行主任务{}子任务{}", subtask.getMainTaskNum(), subtask.getSubTaskNum());
-//        }
     }
 
 
