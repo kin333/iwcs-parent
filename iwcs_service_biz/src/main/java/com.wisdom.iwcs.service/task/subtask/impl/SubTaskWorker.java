4c4f4a96f33e6ec70011bac6ba2d8bcb128e3b32
ted <ted.ma@wisdom56.com>
2019-07-10 13:42:09 +0800

    子任务process 过程出现异常需轮训重试

--- iwcs_service_biz/src/main/java/com.wisdom.iwcs.service/task/subtask/impl/SubTaskWorker.java
+++ iwcs_service_biz/src/main/java/com.wisdom.iwcs.service/task/subtask/impl/SubTaskWorker.java
@@ -105,13 +105,23 @@ public class SubTaskWorker extends AbstractTaskWorker {
 
     @Override
     public void process() {
-        try {
-            IwcsPublicService iwcsPublicService = (IwcsPublicService) SpringContextUtils.getBean("iwcsPublicService");
-            iwcsPublicService.sendInfoBySubTaskNum(subTask.getSubTaskNum());
-        } catch (Exception e) {
-
-
+        while (true) synchronized (waitLock) {
+            try {
+                IwcsPublicService iwcsPublicService = (IwcsPublicService) SpringContextUtils.getBean("iwcsPublicService");
+                iwcsPublicService.sendInfoBySubTaskNum(subTask.getSubTaskNum());
+                break;
+            } catch (Exception e) {
+                logger.error("子任务发送失败{}", subTask.getSubTaskNum());
+                e.printStackTrace();
+                try {
+                    waitLock.wait(1000 * 3);
+                } catch (InterruptedException e1) {
+                    logger.error("子任务发送失败后，尝试休眠失败{}", subTask.getSubTaskNum());
+                    e1.printStackTrace();
+                }
+            }
         }
+
     }
 
     public SubTask getSubTask() {
