5f8908448b08cb96f66f9c690acf2f53ea701c08
songningning <songningning@wisdom56.com>
2019-10-14 10:43:27 +0800

    修改超越线体缓存区补充空货架前置条件

--- iwcs_service_biz/src/main/java/com.wisdom.iwcs.service/task/conditions/conditonHandler/EmptyPodLockForAgingCacheHandler.java
+++ iwcs_service_biz/src/main/java/com.wisdom.iwcs.service/task/conditions/conditonHandler/EmptyPodLockForAgingCacheHandler.java
@@ -9,8 +9,8 @@ import org.springframework.stereotype.Service;
 import java.util.Arrays;
 
 /**
- * 缓存区补充空货架前置条件--锁定老化区的一个空货架
- * @author han
+ * 缓存区补充空货架前置条件--锁定老化区的一个空货架 (优先老化区缓存区，其次老化区)
+ * @author snn
  */
 @Service
 public class EmptyPodLockForAgingCacheHandler implements IConditionHandler {
@@ -20,11 +20,17 @@ public class EmptyPodLockForAgingCacheHandler implements IConditionHandler {
     @Override
     public boolean handleCondition(SubTaskCondition subTaskCondition) {
 
-        AreaCondition areaCondition = new AreaCondition();
-        //查找老化区缓存区的空货架
-            areaCondition.setBizType(InspurBizConstants.BizTypeConstants.AGINGCACHEAREA);
+        AreaCondition areaCondition1 = new AreaCondition();
+        //优先查找老化区缓存区
+        areaCondition1.setBizType(InspurBizConstants.BizTypeConstants.AGINGCACHEAREA);
+        AreaCondition areaCondition2 = new AreaCondition();
+        //其次查找老化区
+        areaCondition2.setArea(InspurBizConstants.OperateAreaCodeConstants.AGINGREA);
 
-        return baseLockEmptyPodService.handleConditionService(subTaskCondition, Arrays.asList(areaCondition), InspurBizConstants.InStock.NO_GOODS);
+        return baseLockEmptyPodService.handleConditionService(subTaskCondition,
+                Arrays.asList(areaCondition1, areaCondition2),
+                InspurBizConstants.InStock.NO_GOODS);
+
     }
 
     @Override
