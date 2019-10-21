e5c9ce6584bbb8e008d34b3825f7a975f17704a0
wuxi <2416993649@qq.com>
2019-10-21 15:14:16 +0800

    改错

--- iwcs_webservice/src/main/java/com/wisdom/controller/task/MesnoticeController.java
+++ iwcs_webservice/src/main/java/com/wisdom/controller/task/MesnoticeController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Random;
 
-@RestController("/api/wisdom/agvHandlingTask")
+@RestController()
 public class MesnoticeController {
     Logger logger = LoggerFactory.getLogger(AspectConfiguration.class);
 
@@ -29,59 +29,59 @@ public class MesnoticeController {
      @Autowired
     AgvHandlingTaskController agvHandlingTaskController;
         //到达起点
-    @RequestMapping("/arriveSrcWb")
+    @RequestMapping("/N2/http/interface.ms?model=AGV&method=arriveSrcWb")
     MesResult arriveSrcWb()
     {
      logger.info("小车到达起点");
         return new MesResult() ;
     }
     //离开起点
-    @RequestMapping("/leaveSrcWb")
+    @RequestMapping("/N2/http/interface.ms?model=AGV&method=leaveSrcWb")
     MesResult leaveSrcWb()
     {
         logger.info("小车离开起点");
         return new MesResult();
     }
     //到达终点
-    @RequestMapping("/arriveDestWb")
+    @RequestMapping("/N2/http/interface.ms?model=AGV&method=arriveDestWb")
     MesResult arriveDestWb()
     {
         logger.info("小车到达终点");
         return new MesResult();
     }
    //AGV到达（机械臂）等待点(通知开/关围栏)
-    @RequestMapping("/arriveDestWbWaitPort")
+    @RequestMapping("/N2/http/interface.ms?model=AGV&method=arriveDestWbWaitPort")
     MesResult arriveDestWbWaitPort()
     {
         logger.info("小车到达等待点");
         return new MesResult() ;
     }
     //MES通知可从等待点进入终点（围栏已开/围栏已关）
-    @PostMapping("/conWaitToDestWb")
-    MesResult conWaitToDestWb(@RequestBody MesBaseRequest<ArriveDestWbWaitPortRequest> mesBaseRequest)
-    {
-        logger.info("MES接受的数据:{}",mesBaseRequest.getData().toString());
-        ConWaitToDestWbRequest conWaitToDestWbRequest = new ConWaitToDestWbRequest();
-        conWaitToDestWbRequest.setTaskCode(mesBaseRequest.getData().getTaskCode());
-        String taskcode  =conWaitToDestWbRequest.getTaskCode();
-        MainTask mainTask = mainTaskMapper.selectByMainTaskNum(taskcode);
-
-        if(mesBaseRequest.getData().getDoorAction().equals("1"))
-        {
-            conWaitToDestWbRequest.setDoorStatus("1");
-            logger.info("MES设置围栏状态已开");
-        }
-        if(mesBaseRequest.getData().getDoorAction().equals("2"))
-        {
-            conWaitToDestWbRequest.setDoorStatus("2");
-        }
-        MesBaseRequest<ConWaitToDestWbRequest> mesBaseRequest1=new MesBaseRequest("10006",conWaitToDestWbRequest);
-        MesResult mesResult =agvHandlingTaskController.conWaitToDestWb(mesBaseRequest1);
-        // MesResult mesResult = mesRequestService.conWaitToDestWb( conWaitToDestWbRequest,"10006");
-        logger.info("任务{}通知可从等待点进入终点的返回值为：{}",taskcode,mesResult.toString());
-        return  mesResult;
-
-    }
+//    @PostMapping("/conWaitToDestWb")
+//    MesResult conWaitToDestWb(@RequestBody MesBaseRequest<ArriveDestWbWaitPortRequest> mesBaseRequest)
+//    {
+//        logger.info("MES接受的数据:{}",mesBaseRequest.getData().toString());
+//        ConWaitToDestWbRequest conWaitToDestWbRequest = new ConWaitToDestWbRequest();
+//        conWaitToDestWbRequest.setTaskCode(mesBaseRequest.getData().getTaskCode());
+//        String taskcode  =conWaitToDestWbRequest.getTaskCode();
+//        MainTask mainTask = mainTaskMapper.selectByMainTaskNum(taskcode);
+//
+//        if(mesBaseRequest.getData().getDoorAction().equals("1"))
+//        {
+//            conWaitToDestWbRequest.setDoorStatus("1");
+//            logger.info("MES设置围栏状态已开");
+//        }
+//        if(mesBaseRequest.getData().getDoorAction().equals("2"))
+//        {
+//            conWaitToDestWbRequest.setDoorStatus("2");
+//        }
+//        MesBaseRequest<ConWaitToDestWbRequest> mesBaseRequest1=new MesBaseRequest("10006",conWaitToDestWbRequest);
+//        MesResult mesResult =agvHandlingTaskController.conWaitToDestWb(mesBaseRequest1);
+//        // MesResult mesResult = mesRequestService.conWaitToDestWb( conWaitToDestWbRequest,"10006");
+//        logger.info("任务{}通知可从等待点进入终点的返回值为：{}",taskcode,mesResult.toString());
+//        return  mesResult;
+//
+//    }
 }
 
 
