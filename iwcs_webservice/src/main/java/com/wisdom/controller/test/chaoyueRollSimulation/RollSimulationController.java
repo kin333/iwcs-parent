eff806a81c11d6b570d5879467ca52748b59ae4a
liuningning <1754102225@qq.com>
2019-10-23 13:59:56 +0800

    '超越滚筒模拟测试'

---
+++ iwcs_webservice/src/main/java/com/wisdom/controller/test/chaoyueRollSimulation/RollSimulationController.java
@@ -0,0 +1,10 @@
+package com.wisdom.controller.test.chaoyueRollSimulation;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/wisdom/autoProductionLine/supplyAndRecyle")
+public class RollSimulationController {
+}
