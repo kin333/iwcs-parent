3b566816ce34182be97f1c11610c3628c1db46e8
han <hanyueqian@wisdom56.com>
2019-11-15 09:18:45 +0800

    修改前置条件请求时间

--- iwcs_service_biz/src/main/java/com.wisdom.iwcs.service/task/subtask/impl/SubTaskWorker.java
+++ iwcs_service_biz/src/main/java/com.wisdom.iwcs.service/task/subtask/impl/SubTaskWorker.java
@@ -69,7 +69,7 @@ public class SubTaskWorker extends AbstractTaskWorker {
                     System.out.println("sub task is going to wait " + waitLock);
                     if (! isRunnable()) {
                         logger.info("Task {}, subtask: {} is gonging to wait 10*1000, go...", subTask.getMainTaskNum(), subTask.getSubTaskNum());
-                        waitLock.wait(10 * 1000 * 1);
+                        waitLock.wait(5 * 1000 * 1);
                         logger.info("Task thread start!");
                     } else{
                         logger.info("Task {}, subtask: {} become runnable, go...", subTask.getMainTaskNum(), subTask.getSubTaskNum());
